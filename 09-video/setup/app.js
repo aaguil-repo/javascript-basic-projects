d17739996d0009c79640f45a285b0e8221e41006
Andy <131629697+aaguil-repo@users.noreply.github.com>
2024-05-01 19:47:25 -0400

    video toggle complete

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -7,5 +7,22 @@ const video = document.querySelector('.video-container');
 
 btn.addEventListener('click', function()
 {
+    if(!btn.classList.contains('slide'))
+    {
+        btn.classList.add('slide');
+        video.pause();
+    }
+    else
+    {
+        btn.classList.remove('slide');
+        video.play();
+    }
+});
 
-})
+// Preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function()
+{
+    preloader.classList.add('hide-preloader');
+});
